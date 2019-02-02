ddeb5e42bf191fda1504e531a8b4bd9f150d61a0
Avaer Kazmer <a@modules.io>
2019-02-02 00:14:34 +0000

    Add symbol renames to archive-ml.sh

--- scripts/archive-ml.sh
+++ scripts/archive-ml.sh
@@ -9,3 +9,5 @@ chmod +x gen-webrtc-ar.sh
 rm -f libwebrtc.a
 node ./gen-libwebrtc-mri.js >libwebrtc.mri
 "$MLSDK/tools/toolchains/bin/aarch64-linux-android-ar" -M <libwebrtc.mri
+
+"$MLSDK/tools/toolchains/bin/aarch64-linux-android-objcopy" --redefine-sym isqrt32=webrtc_isqrt32 --redefine-sym silk_insertion_sort_increasing=webrtc_silk_insertion_sort_increasing --redefine-sym silk_insertion_sort_increasing_all_values_int16=webrtc_silk_insertion_sort_increasing_all_values_int16 libwebrtc.a
