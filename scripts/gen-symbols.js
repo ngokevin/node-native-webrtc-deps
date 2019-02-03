8c6d3c84a5f46f13bf981c51ed43484275f82179
Avaer Kazmer <a@modules.io>
2019-02-03 01:22:56 +0000

    Reimplement gen-skips script as gen-symbols

---
+++ scripts/gen-symbols.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const s = fs.readFileSync('error.txt', 'utf8');
+const ls = s.split('\n');
+// process.stdout.write('gawk \'{if (');
+const index = {};
+
+for (let i = 0; i < ls.length; i++) {
+  const l = ls[i];
+  const match = l.match(/multiple definition of `(.+?)'/);
+  if (match && !index[match[1]]) {
+    console.log(`${match[1]} webrtc_${match[1]}`);
+    index[match[1]] = true;
+  }
+}
+// process.stdout.write(') {print "skip"}}\'');
